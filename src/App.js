03ad610752d38f34d18c3450715e5cc030957b34
miguel aguedo <aguedomiguel242@gmail.com>
2020-08-21 15:15:41 -0500

    mi publicacion 1

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://dtodo2022.milaulas.com/course/view.php?id=2"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Moodle
+        </a>
+        <b
+          className="App-link"
+          href="http://www.dtodo.infinityfreeapp.com/elementor-46/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          WordPress
+        </b>
+      </header>
+    </div>
+  );
+}
+
+export default App;
